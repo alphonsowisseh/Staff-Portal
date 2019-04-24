3acef75168c85fac68c019005a315397bec5d3a5
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 15:24:20 -0400

    Import Home and Staff pages and Router for link to work

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+import Home from './components/pages/Home'
+import Staff from './components/pages/Staff'
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <h1>Hello</h1>
+    <Navbar />
+    <Route path='/' exact={true} component={Home}></Route>
+    <Route path='/staff' exact={true} component={Staff}></Route>
     </div>
+    </Router>
   );
 }
 
