889060c7875b20f5ae58c1d92c4c83b6521cdc99
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 15:44:11 -0400

    import login and remove closing route tags

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import Staff from './components/pages/Staff'
+import Login from './components/auth/Login'
 
 function onAuthRequired({history}){
   history.push('/login');
@@ -20,8 +21,8 @@ function App() {
     <div className="App">
     <Navbar />
     <div className="container">
-    <Route path='/' exact={true} component={Home}></Route>
-    <SecureRoute path='/staff' exact={true} component={Staff}></Route>
+    <Route path='/' exact={true} component={Home} />
+    <SecureRoute path='/staff' exact={true} component={Staff} />
     <Route path='/login' render={() => <Login baseUrl='https://dev-750920.okta.com' />} />
     <Route path='/implicit/callback' component={ImplicitCallback} />
     </div>
