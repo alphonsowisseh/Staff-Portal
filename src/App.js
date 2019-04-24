861d09028c61570e29b110d90403a37eefef47e2
Alphonso Wisseh <alphonso.wissehjr@gmail.com>
2019-04-24 15:39:34 -0400

    Add secure route with okta and redirect for unauthenticated users

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import './App.css';
 import {BrowserRouter as Router, Route} from 'react-router-dom'
+import { Security, SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import Navbar from './components/layout/Navbar'
 import Home from './components/pages/Home'
 import Staff from './components/pages/Staff'
 
+function onAuthRequired({history}){
+  history.push('/login');
+}
+
 function App() {
   return (
     <Router>
+      <Security issuer='https://dev-750920.okta.com/oauth2/default'
+                  client_id='0oai5sphirY7PZiD7356'
+                  redirect_uri={window.location.origin + '/implicit/callback'}
+                  onAuthRequired={onAuthRequired} >
     <div className="App">
     <Navbar />
     <div className="container">
     <Route path='/' exact={true} component={Home}></Route>
-    <Route path='/staff' exact={true} component={Staff}></Route>
+    <SecureRoute path='/staff' exact={true} component={Staff}></Route>
+    <Route path='/login' render={() => <Login baseUrl='https://dev-750920.okta.com' />} />
+    <Route path='/implicit/callback' component={ImplicitCallback} />
     </div>
     </div>
+    </Security>
     </Router>
   );
 }
